51786568ce34914a0f2d2b477a964e3fd0bbe354
UpSnow <arthurcel2002@gmail.com>
2026-01-18 06:33:03 -0300

    ⭐feat(Tela):T Tela login quase finalizada

--- Sweet-Delights/src/pages/Login/Login.jsx
+++ Sweet-Delights/src/pages/Login/Login.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import './Login.css'
 import Input from "../../components/input/input";
+import Button from "../../components/Button/Button";
 
-const Login = () =>{
-    return(
-        <Input
-        label={"arthur"}
-        type="texto"
-        placeholder={"ARTHUR"}
 
-        ></Input>
+const Login = () => {
+    return (
+        <div className="container">
+            <h1>Login</h1>
+            <Input
+                label={"Email"}
+                type="email"
+                placeholder={"seu@email.com"}
+
+            />
+            <Input
+                label={"Senha"}
+                type="password"
+                placeholder={"********"}
+
+            />
+            <Button variant="primary">Entrar</Button>
+
+            <p className="login-text">
+                Não tem uma conta?{" "}
+                <a href="/cadastro" className="login-link">
+                    Cadastre-se
+                </a>
+            </p>
+        </div>
 
     )
 
