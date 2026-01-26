b4f9f4c5f613ee7a8ca1947cf1110a0b952f6bf2
natanael santos <natanaelsantoss2br@gmail.com>
2026-01-26 17:19:01 -0300

    feat:Carrinho de compra dos produtos

--- Sweet-Delights/src/Routes/Routes.jsx
+++ Sweet-Delights/src/Routes/Routes.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import LayoutPage from "../components/LayoutPage/LayoutPage";
 import Details from "../pages/DetailsProduct/Details.jsx";
-
+import CartProduct from "../pages/CartProduct/CartProduct.jsx";
 
 const AppRoutes = () => {
     return(
@@ -12,11 +12,11 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/layout" element={<LayoutPage/>}/>
-                 <Route path="/details" element={<Details/>} />
+                <Route path="/details" element={<Details/>}/>
+                <Route path="/cartProduct" element={<CartProduct/>}/>
             </Routes>
         </Router>
     )
-
 }
 
 export default AppRoutes
---
+++ Sweet-Delights/src/pages/CartProduct/CartProduct.jsx
@@ -0,0 +1,11 @@
+import "./CartProduct.css"
+
+function CartProduct (){
+    return (
+        <main>
+            <h1>Teste</h1>
+        </main>
+    )
+}
+
+export default CartProduct
