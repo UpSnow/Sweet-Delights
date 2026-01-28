869b93098331590f7eab223096c53c28ef434665
natanael santos <natanaelsantoss2br@gmail.com>
2026-01-27 23:07:16 -0300

    feat:pagina de detalhes do produto em andamento...

--- Sweet-Delights/src/pages/CartProduct/CartProduct.jsx
+++ Sweet-Delights/src/pages/CartProduct/CartProduct.jsx
@@ -2,9 +2,42 @@ import "./CartProduct.css"
 
 function CartProduct (){
     return (
-        <main>
-            <h1>Teste</h1>
-        </main>
+        <div>
+            <section>
+                <h2>Carrinho</h2>
+                <div>
+                    <img src="" alt="" />
+                    <h1>Brownie</h1>
+                    <p>5,00</p>
+                </div>
+                <div>
+                    <button>+</button>
+                    <p>1</p>
+                    <button>-</button>
+                </div>
+            </section>
+            <section>
+                <p>Forma de pagamento</p>
+                <h1>SubTotal</h1>
+                <div>
+                    <form>
+                        <label>
+                            <img src="" alt="" />
+                            <input type="radio" name="pagamento" value="1"/>
+                        </label>
+                        <label>
+                            <img src="" alt="" />
+                            <input type="radio" name="pagamento" value="2"/>
+                        </label>
+                        <label>
+                            <img src="" alt="" />
+                            <input type="radio" name="pagamento" value="3"/>
+                        </label>
+                        <button type="submit">Finalizar</button>
+                    </form>
+                </div>
+            </section>       
+        </div>
     )
 }
 
