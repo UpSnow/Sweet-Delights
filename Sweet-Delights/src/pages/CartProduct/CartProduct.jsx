4c4b11142a372e5c91730f5c62414fb037b67e43
natanael santos <natanaelsantoss2br@gmail.com>
2026-01-29 23:52:12 -0300

    feat: cart product style em andamento

--- Sweet-Delights/src/pages/CartProduct/CartProduct.jsx
+++ Sweet-Delights/src/pages/CartProduct/CartProduct.jsx
@@ -11,19 +11,22 @@ function CartProduct (){
         <div>
             <section>
                 <h2>Carrinho</h2>
-                <div>
-                    <h1>Brownie</h1>
-                    <p>5,00</p>
-                </div>
-                <div>
-                    <h2>Produto X</h2>
-
-                    <QuantityControl
-                        value={quantity}
-                        min={1}
-                        max={stock}
-                        onChange={setQuantity}
-                    />
+                <div className="card-product-cart">
+                    <div>
+                        <img className="icon-product" src="" alt="" width="10px"/>
+                    </div>
+                    <div>
+                        <h1>Brownie</h1>
+                        <span>5,00</span>
+                    </div>
+                    <div>
+                        <QuantityControl className="teste"
+                            value={quantity}
+                            min={1}
+                            max={stock}
+                            onChange={setQuantity}
+                        />                            
+                    </div>         
                 </div>
             </section>
             <section>
@@ -32,14 +35,24 @@ function CartProduct (){
                 <div>
                     <form>
                         <label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card-fill" viewBox="0 0 16 16">
+                                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1"/>
+                            </svg>
                             <input type="radio" name="pagamento" value="1"/>
                         </label>
                         <label>
-
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0"/>
+                                <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z"/>
+                                <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z"/>
+                                <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567"/>
+                            </svg>
                             <input type="radio" name="pagamento" value="2"/>
                         </label>
                         <label>
-
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="20px">
+                                <path d="M306.4 356.5C311.8 351.1 321.1 351.1 326.5 356.5L403.5 433.5C417.7 447.7 436.6 455.5 456.6 455.5L471.7 455.5L374.6 552.6C344.3 582.1 295.1 582.1 264.8 552.6L167.3 455.2L176.6 455.2C196.6 455.2 215.5 447.4 229.7 433.2L306.4 356.5zM326.5 282.9C320.1 288.4 311.9 288.5 306.4 282.9L229.7 206.2C215.5 191.1 196.6 184.2 176.6 184.2L167.3 184.2L264.7 86.8C295.1 56.5 344.3 56.5 374.6 86.8L471.8 183.9L456.6 183.9C436.6 183.9 417.7 191.7 403.5 205.9L326.5 282.9zM176.6 206.7C190.4 206.7 203.1 212.3 213.7 222.1L290.4 298.8C297.6 305.1 307 309.6 316.5 309.6C325.9 309.6 335.3 305.1 342.5 298.8L419.5 221.8C429.3 212.1 442.8 206.5 456.6 206.5L494.3 206.5L552.6 264.8C582.9 295.1 582.9 344.3 552.6 374.6L494.3 432.9L456.6 432.9C442.8 432.9 429.3 427.3 419.5 417.5L342.5 340.5C328.6 326.6 304.3 326.6 290.4 340.6L213.7 417.2C203.1 427 190.4 432.6 176.6 432.6L144.8 432.6L86.8 374.6C56.5 344.3 56.5 295.1 86.8 264.8L144.8 206.7L176.6 206.7z"/>
+                            </svg>
                             <input type="radio" name="pagamento" value="3"/>
                         </label>
                         <button type="submit">Finalizar</button>
